ca44da92cbd210e0660630e26ec9b18f2143e403
tjdus <dusml0277@gmail.com>
2025-05-09 14:19:19 +0900

    keyword search

--- apps/naver_api/urls.py
+++ apps/naver_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.naver_api.views.search_news import SearchNewsView
+from apps.naver_api.views.search_news import SearchNewsView, KeyWordNewsView
 
 urlpatterns = [
   path('news', SearchNewsView.as_view(), name='news'),
+  path('keyword-news', KeyWordNewsView.as_view(), name='keyword-news'),
 ]
--- apps/naver_api/views/search_news.py
+++ apps/naver_api/views/search_news.py
@@ -5,6 +5,7 @@ from konlpy.tag import Okt
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.naver_api.serializers.article import ArticleSerializer
 from config import settings
 
 NAVER_CLIENT_ID = settings.NAVER_CLIENT_ID
@@ -13,7 +14,7 @@ NAVER_CLIENT_SECRET = settings.NAVER_CLIENT_SECRET
 class SearchNewsView(APIView):
     def get(self, request):
         query = request.GET.get('query', "")
-        display = request.GET.get('display', 10)
+        display = request.GET.get('display', 100)
 
         url = f"https://openapi.naver.com/v1/search/news.json?query={query}&display={display}&start=1&sort=sim"
 
@@ -52,3 +53,28 @@ class SearchNewsView(APIView):
         word_cloud_data = [{"text": word, "value": freq} for word, freq in word_freq.most_common(10)]
 
         return word_cloud_data
+
+class KeyWordNewsView(APIView):
+    def get(self, request):
+        query = request.GET.get('query', "")
+        keyword = request.GET.get('keyword', "")
+        display = request.GET.get('display', 10)
+
+        url = f"https://openapi.naver.com/v1/search/news.json?query=\"{query}\"+{keyword}&display={display}&start=1&sort=sim"
+
+        headers = {
+            "X-Naver-Client-Id": NAVER_CLIENT_ID,
+            "X-Naver-Client-Secret": NAVER_CLIENT_SECRET
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            articles = data['items']
+
+            serializer = ArticleSerializer(articles, many=True)
+
+            return Response(serializer.data)
+        else:
+            return Response({"error": "Failed to fetch data from Naver API"}, status=500)
