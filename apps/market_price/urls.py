83b3661b29505e9d3bc5785fd31cc521195b323d
tjdus <dusml0277@gmail.com>
2025-05-15 18:42:43 +0900

    Add exchange-rate

--- apps/market_price/urls.py
+++ apps/market_price/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from apps.market_price.views.carbon_price import CarbonPriceView
+from apps.market_price.views.exchange_rate import ExchangeRateView
 
 urlpatterns = [
   path('carbon-price', CarbonPriceView.as_view(), name='carbon_price'),
+  path('exchage-rate', ExchangeRateView.as_view(), name='exchange_rate'),
 ]
