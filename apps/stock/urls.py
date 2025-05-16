a8fc3f453f4d866248e7b6497965f4d8a1f43215
tjdus <dusml0277@gmail.com>
2025-05-16 14:41:38 +0900

    Add urls

--- apps/stock/urls.py
+++ apps/stock/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from apps.stock.views.financial_data import FinancialDataView
-from apps.stock.views.stock import StockView
+from apps.stock.views.stock import StockView, StockHistoryView
 
 urlpatterns = [
   path('stock', StockView.as_view(), name='stock'),
+  path('stock-history', StockHistoryView.as_view(), name='stock-history'),
   path('financial-data', FinancialDataView.as_view(), name='financial-data'),
 ]
